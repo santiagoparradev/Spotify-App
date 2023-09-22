4795c3e3d1567361b092ad22d3eff60898ed33aa
unknown <NiCo_parra201>
2023-09-22 14:13:58 -0500

    changes for the branch

--- force-app/main/default/lwc/popularity/popularity.js
+++ force-app/main/default/lwc/popularity/popularity.js
@@ -5,30 +5,28 @@ import POPULARITY_FIELD from "@salesforce/schema/Track__c.Popularity__c";
 export default class Popularity extends LightningElement {
   @api recordId;
   message;
-  color = null;
+
   @wire(getRecord, {
     recordId: "$recordId",
-    fields: [POPULARITY_FIELD],
+    fields: [POPULARITY_FIELD]
   })
-  trackRecord({data, error}) {
-    
+  trackRecord({ data }) {
     if (data) {
       const popularity = getFieldValue(data, POPULARITY_FIELD);
+      let color = "#40f702";
       if (popularity <= 30) {
-        document.documentElement.style.setProperty('--titleColor', '#d62023');
-        this.message = "THIS SONG IS BAD ACCORDING TO MOST PEOPLE"; 
+        color = "#d62023";
+        this.message = "this song is bad according to most people";
       } else if (popularity <= 50) {
-        document.documentElement.style.setProperty('--titleColor', '#d1d62d');
-        this.message = "NOT THE BEST SONG";
+        color = "#d1d62d";
+        this.message = "not the best song";
       } else if (popularity <= 70) {
-        document.documentElement.style.setProperty('--titleColor', '#72b533');
-        this.message = "GREAT SONG";
+        color = "#72b533";
+        this.message = "great song";
       } else {
-        document.documentElement.style.setProperty('--titleColor', '#40f702');
-        this.message = "THIS SONG IS FIRE";
-      } 
-    } else if (error) {
-        message = "error";
+        this.message = "this song is fire";
+      }
+      document.documentElement.style.setProperty("--titleColor", color);
     }
   }
 }
