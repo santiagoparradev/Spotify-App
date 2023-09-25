f487df7f7de8c918700ccc0d7115acdda414bae6
unknown <NiCo_parra201>
2023-09-25 10:39:53 -0500

    missing changes

--- force-app/main/default/lwc/popularity/__tests__/popularity.test.js
+++ force-app/main/default/lwc/popularity/__tests__/popularity.test.js
@@ -3,52 +3,41 @@ import Popularity from "c/popularity";
 import { getRecord } from "lightning/uiRecordApi";
 describe("c-popularity", () => {
   afterEach(() => {
-    // The jsdom instance is shared across test cases in a single file so reset the DOM
     while (document.body.firstChild) {
       document.body.removeChild(document.body.firstChild);
     }
   });
   it("popularity is minor to 30", () => {
-    // Arrange
     const element = createElement("c-popularity", {
       is: Popularity
     });
-    // Act
+
     document.body.appendChild(element);
 
-    //Emit mock record into the wired field
     const mock = { fields: { Popularity__c: { value: 15 } } };
     getRecord.emit(mock);
 
-    //Resolve a promise
     return Promise.resolve().then(() => {
       const content = element.shadowRoot.querySelector("p");
 
-      // Assert
-      // const div = element.shadowRoot.querySelector('div');
       expect(content.textContent).toBe(
         "this song is bad according to most people"
       );
     });
   });
   it("popularity is high", () => {
-    // Arrange
     const element = createElement("c-popularity", {
       is: Popularity
     });
-    // Act
+
     document.body.appendChild(element);
 
-    //Emit mock record into the wired field
     const mock = { fields: { Popularity__c: { value: 101 } } };
     getRecord.emit(mock);
 
-    //Resolve a promise
     return Promise.resolve().then(() => {
       const content = element.shadowRoot.querySelector("p");
 
-      // Assert
-      // const div = element.shadowRoot.querySelector('div');
       expect(content.textContent).toBe("this song is fire");
     });
   });
