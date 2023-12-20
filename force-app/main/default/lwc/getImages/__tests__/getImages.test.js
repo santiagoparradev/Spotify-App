507e9e47d2e39a20f03ae84f500a05bcac3d7453
unknown <NiCo_parra201>
2023-12-20 16:33:18 -0500

    #81 make the image component be able to show all the images

--- force-app/main/default/lwc/getImages/__tests__/getImages.test.js
+++ force-app/main/default/lwc/getImages/__tests__/getImages.test.js
@@ -29,18 +29,31 @@ describe("c-getImages", () => {
 
   it("the url expect is equal to string in tobe", async () => {
     // Arrange
-    findUrl.mockResolvedValue("https://www.url.com");
+    const urls = [
+      "https://www.url.com",
+      "https://www.url2.com",
+      "https://www.url3.com"
+    ];
+    findUrl.mockResolvedValue(urls);
+
     const element = createElement("c-get-images", {
       is: getImages
     });
+
     document.body.appendChild(element);
     const mock = { fields: { SpotifyId__c: { value: "123456" } } };
-
     // Act
     getRecord.emit(mock);
     await flushPromises();
     // Assert
-    const urlImage = element.shadowRoot.querySelector("img");
-    expect(urlImage.src).toBe("https://www.url.com");
+    const urlImages = element.shadowRoot.querySelectorAll(
+      "lightning-carousel-image"
+    );
+    //expect(urlImage.src).toBe("https://www.url.com");
+    expect(urlImages.length).toBe(urls.length);
+    // Verificar que cada imagen tenga la URL esperada
+    urlImages.forEach((urlImage, index) => {
+      expect(urlImage.src).toBe(urls[index]);
+    });
   });
 });
